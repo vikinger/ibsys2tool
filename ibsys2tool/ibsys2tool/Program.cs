21c0c01250c6b8123fe036c3fc3c77aa96afe917
Viktor Plotnikow <viktor-p89@web.de>
2013-10-10 13:31:39 +0200

    test2

--- ibsys2tool/ibsys2tool/Program.cs
+++ ibsys2tool/ibsys2tool/Program.cs
@@ -14,6 +14,7 @@ namespace ibsys2tool
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
