cab9a43130fef0ba1a6fb74c15def1eff908ce77
FelixA <felixaber@gmail.com>
2013-10-14 11:59:33 +0200

    Kommentar in main.cs

--- ibsys2tool/ibsys2tool/Program.cs
+++ ibsys2tool/ibsys2tool/Program.cs
@@ -14,6 +14,7 @@ namespace ibsys2tool
         [STAThread]
         static void Main()
         {
+            //öffnet den Startbildschirm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
