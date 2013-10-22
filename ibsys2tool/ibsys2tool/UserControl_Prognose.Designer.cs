eb455c046c71bdbe16940fa32c6ccb50c94cb352
muad1011 <muad1011@hs-karlsruhe.de>
2013-10-22 19:47:02 +0200

    Design, Multilanguage-Fähigkeit

--- ibsys2tool/ibsys2tool/UserControl_Prognose.Designer.cs
+++ ibsys2tool/ibsys2tool/UserControl_Prognose.Designer.cs
@@ -28,24 +28,423 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl_Prognose));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.udP1n = new System.Windows.Forms.NumericUpDown();
+            this.udP2n = new System.Windows.Forms.NumericUpDown();
+            this.udP3n = new System.Windows.Forms.NumericUpDown();
+            this.udP1r = new System.Windows.Forms.NumericUpDown();
+            this.udP2r = new System.Windows.Forms.NumericUpDown();
+            this.udP3r = new System.Windows.Forms.NumericUpDown();
+            this.udP1n1 = new System.Windows.Forms.NumericUpDown();
+            this.udP2n1 = new System.Windows.Forms.NumericUpDown();
+            this.udP3n1 = new System.Windows.Forms.NumericUpDown();
+            this.udP1n2 = new System.Windows.Forms.NumericUpDown();
+            this.udP2n2 = new System.Windows.Forms.NumericUpDown();
+            this.udP3n2 = new System.Windows.Forms.NumericUpDown();
+            this.udP3n3 = new System.Windows.Forms.NumericUpDown();
+            this.udP2n3 = new System.Windows.Forms.NumericUpDown();
+            this.udP1n3 = new System.Windows.Forms.NumericUpDown();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbPathToXml = new System.Windows.Forms.TextBox();
+            this.btnOpenXml = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1r)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2r)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3r)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 20);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Prognose";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // btnCalculate
+            // 
+            resources.ApplyResources(this.btnCalculate, "btnCalculate");
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // udP1n
+            // 
+            resources.ApplyResources(this.udP1n, "udP1n");
+            this.udP1n.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP1n.Name = "udP1n";
+            this.udP1n.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP2n
+            // 
+            resources.ApplyResources(this.udP2n, "udP2n");
+            this.udP2n.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP2n.Name = "udP2n";
+            this.udP2n.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP3n
+            // 
+            resources.ApplyResources(this.udP3n, "udP3n");
+            this.udP3n.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP3n.Name = "udP3n";
+            this.udP3n.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP1r
+            // 
+            resources.ApplyResources(this.udP1r, "udP1r");
+            this.udP1r.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP1r.Name = "udP1r";
+            this.udP1r.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // udP2r
+            // 
+            resources.ApplyResources(this.udP2r, "udP2r");
+            this.udP2r.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP2r.Name = "udP2r";
+            this.udP2r.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // udP3r
+            // 
+            resources.ApplyResources(this.udP3r, "udP3r");
+            this.udP3r.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP3r.Name = "udP3r";
+            this.udP3r.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // udP1n1
+            // 
+            resources.ApplyResources(this.udP1n1, "udP1n1");
+            this.udP1n1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP1n1.Name = "udP1n1";
+            this.udP1n1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP2n1
+            // 
+            resources.ApplyResources(this.udP2n1, "udP2n1");
+            this.udP2n1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP2n1.Name = "udP2n1";
+            this.udP2n1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP3n1
+            // 
+            resources.ApplyResources(this.udP3n1, "udP3n1");
+            this.udP3n1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP3n1.Name = "udP3n1";
+            this.udP3n1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP1n2
+            // 
+            resources.ApplyResources(this.udP1n2, "udP1n2");
+            this.udP1n2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP1n2.Name = "udP1n2";
+            this.udP1n2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP2n2
+            // 
+            resources.ApplyResources(this.udP2n2, "udP2n2");
+            this.udP2n2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP2n2.Name = "udP2n2";
+            this.udP2n2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP3n2
+            // 
+            resources.ApplyResources(this.udP3n2, "udP3n2");
+            this.udP3n2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP3n2.Name = "udP3n2";
+            this.udP3n2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP3n3
+            // 
+            resources.ApplyResources(this.udP3n3, "udP3n3");
+            this.udP3n3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP3n3.Name = "udP3n3";
+            this.udP3n3.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP2n3
+            // 
+            resources.ApplyResources(this.udP2n3, "udP2n3");
+            this.udP2n3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP2n3.Name = "udP2n3";
+            this.udP2n3.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // udP1n3
+            // 
+            resources.ApplyResources(this.udP1n3, "udP1n3");
+            this.udP1n3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udP1n3.Name = "udP1n3";
+            this.udP1n3.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // tbPathToXml
+            // 
+            resources.ApplyResources(this.tbPathToXml, "tbPathToXml");
+            this.tbPathToXml.Name = "tbPathToXml";
+            this.tbPathToXml.ReadOnly = true;
+            this.tbPathToXml.TextChanged += new System.EventHandler(this.tbPathToXml_TextChanged);
+            // 
+            // btnOpenXml
+            // 
+            resources.ApplyResources(this.btnOpenXml, "btnOpenXml");
+            this.btnOpenXml.Name = "btnOpenXml";
+            this.btnOpenXml.UseVisualStyleBackColor = true;
+            this.btnOpenXml.Click += new System.EventHandler(this.btnOpenXml_Click);
+            // 
+            // label14
+            // 
+            resources.ApplyResources(this.label14, "label14");
+            this.label14.Name = "label14";
             // 
             // UserControl_Prognose
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.btnOpenXml);
+            this.Controls.Add(this.tbPathToXml);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.udP3n3);
+            this.Controls.Add(this.udP2n3);
+            this.Controls.Add(this.udP1n3);
+            this.Controls.Add(this.udP3n2);
+            this.Controls.Add(this.udP2n2);
+            this.Controls.Add(this.udP1n2);
+            this.Controls.Add(this.udP3n1);
+            this.Controls.Add(this.udP2n1);
+            this.Controls.Add(this.udP1n1);
+            this.Controls.Add(this.udP3r);
+            this.Controls.Add(this.udP2r);
+            this.Controls.Add(this.udP1r);
+            this.Controls.Add(this.udP3n);
+            this.Controls.Add(this.udP2n);
+            this.Controls.Add(this.udP1n);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "UserControl_Prognose";
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1r)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2r)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3r)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP3n3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP2n3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udP1n3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,5 +453,37 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.NumericUpDown udP1n;
+        private System.Windows.Forms.NumericUpDown udP2n;
+        private System.Windows.Forms.NumericUpDown udP3n;
+        private System.Windows.Forms.NumericUpDown udP1r;
+        private System.Windows.Forms.NumericUpDown udP2r;
+        private System.Windows.Forms.NumericUpDown udP3r;
+        private System.Windows.Forms.NumericUpDown udP1n1;
+        private System.Windows.Forms.NumericUpDown udP2n1;
+        private System.Windows.Forms.NumericUpDown udP3n1;
+        private System.Windows.Forms.NumericUpDown udP1n2;
+        private System.Windows.Forms.NumericUpDown udP2n2;
+        private System.Windows.Forms.NumericUpDown udP3n2;
+        private System.Windows.Forms.NumericUpDown udP3n3;
+        private System.Windows.Forms.NumericUpDown udP2n3;
+        private System.Windows.Forms.NumericUpDown udP1n3;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tbPathToXml;
+        private System.Windows.Forms.Button btnOpenXml;
+        private System.Windows.Forms.Label label14;
+
     }
 }
--- ibsys2tool/ibsys2tool/UserControl_Prognose.cs
+++ ibsys2tool/ibsys2tool/UserControl_Prognose.cs
@@ -16,5 +16,33 @@ namespace ibsys2tool
         {
             InitializeComponent();
         }
+
+        private void btnOpenXml_Click(object sender, EventArgs e)
+        {
+            //Öffnet OpenFileDialog
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.OpenFile() != null)
+                {
+                    tbPathToXml.Text = openFileDialog1.FileName;
+                }
+            }
+        }
+
+        private void tbPathToXml_TextChanged(object sender, EventArgs e)
+        {
+            //Wenn Pfad-Textbox leer, ist "Berechnen" nicht möglich
+            if(!String.IsNullOrEmpty(tbPathToXml.Text))
+                btnCalculate.Enabled = true;
+            else
+                btnCalculate.Enabled = false;
+        }
     }
 }
